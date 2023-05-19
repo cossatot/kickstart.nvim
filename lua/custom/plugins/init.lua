51fdaffc3e805bab75a3040e7b302ef1885dda13
Richard Styron <richard.h.styron@gmail.com>
2023-05-19 11:10:28 -0700

    santiam updates

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,7 +3,36 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-	"jeffkreeftmeijer/vim-numbertoggle",
-	"ethanmuller/scratch.vim",
-	"luochen1990/rainbow",
+    "jeffkreeftmeijer/vim-numbertoggle",
+    "ethanmuller/scratch.vim",
+    "luochen1990/rainbow",
+    "ervandew/supertab",
+    "lervag/vimtex",
+    
+    {"vim-pandoc/vim-pandoc",
+    config = function ()
+	    vim.cmd([[
+	    " let g:pandoc#syntax#conceal#use = 0
+	    " let g:pandoc#formatting#mode = 'ha'
+	    let g:pandoc#modules#disabled = ['folding']
+	    ]])
+	end
+	},
+    "vim-pandoc/vim-pandoc-syntax",
+
+    {"preservim/vim-pencil",
+    config = function()
+        vim.cmd([[
+let g:pencil#textwidth = 79
+let g:pencil#conceallevel = 0
+
+augroup pencil
+autocmd!
+autocmd FileType markdown call pencil#init({'wrap': 'hard'})
+autocmd FileType latex call pencil#init({'wrap': 'hard'})
+augroup END
+]])
+end
+}
+
 }
